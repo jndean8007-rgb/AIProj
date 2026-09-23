4110a17c0187b5f9e3d2a98fc0ffa86e31c39e9a
jndean8007 <jndean8007@gmail.com>
2026-09-22 22:41:35 -0700

    testing incomingGG

--- src/torch_llm/main.py
+++ src/torch_llm/main.py
@@ -9,8 +9,8 @@ from torch_llm.inference.runtime import InferenceRuntime
 from torch_llm.model.model_config import ModelConfig
 from torch_llm.training.run_training import run_training
 from torch_llm.training.train_config import TrainConfig
+from torch_llm.path_config import PathConfig
 import torch as t
-
 
 
 #all these paths probably live in configs
@@ -19,33 +19,25 @@ import torch as t
 def main():
     #REFACTOR ALL OF THIS TO A DISTRIBUTOR: TRAIN, INFER, ETC
 
-
-
-    train_path = Path(r'C:\Users\rosie\PycharmProjects\AIBigProject\data\processed\tiny_shake\train')
-
-    eval_path = Path(r'C:\Users\rosie\PycharmProjects\AIBigProject\data\processed\tiny_shake\eval')
+    path_config = PathConfig()
 
     model_config = ModelConfig()
     train_config = TrainConfig()
     tokenizer_config = BPETokenizerConfig()
 
     from_checkpoint = True #True
-    config_load_path = None
-    model_load_path = r"C:\Users\rosie\PycharmProjects\AIBigProject\checkpoints\tinyshake\0.pth"
-    tokenizer_load_path = r"C:\Users\rosie\PycharmProjects\AIBigProject\checkpoints\tinyshake\BPETokenizer"
-    training_log_path = r"C:\Users\rosie\PycharmProjects\AIBigProject\checkpoints\tinyshake\trainingstats"
 
     run_training(
         model_config=model_config,
         train_config=train_config,
         tokenizer_config=tokenizer_config,
-        train_path=train_path,
-        eval_path=eval_path,
-        training_log_path=training_log_path,
+        train_path=path_config.train_path,
+        eval_path=path_config.eval_path,
+        training_log_path=path_config.training_log_path,
         from_checkpoint=from_checkpoint,
-        config_load_path=config_load_path,
-        model_load_path=model_load_path,
-        tokenizer_load_path=tokenizer_load_path,
+        config_load_path=path_config.config_load_path,
+        model_load_path=path_config.model_load_path,
+        tokenizer_load_path=path_config.tokenizer_load_path,
     )
 
 
---
+++ src/torch_llm/path_config.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass
+class PathConfig:
+    train_path: Path | None = Path(r'C:\Users\rosie\PycharmProjects\AIBigProject\data\processed\tiny_shake\train')
+    eval_path: Path | None = Path(r'C:\Users\rosie\PycharmProjects\AIBigProject\data\processed\tiny_shake\eval')
+    training_log_path: Path | None = Path(r"C:\Users\rosie\PycharmProjects\AIBigProject\checkpoints\tinyshake\trainingstats")
+    model_load_path: Path| None = Path(r"C:\Users\rosie\PycharmProjects\AIBigProject\checkpoints\tinyshake\0.pth")
+    tokenizer_load_path: Path | None = Path(r"C:\Users\rosie\PycharmProjects\AIBigProject\checkpoints\tinyshake\BPETokenizer")
+    config_load_path: Path | None = None
+
+    def __post_init__(self):
+        assert type(self.train_path) == type(self.eval_path) # upon distribution no longer valid
+        assert type(self.training_log_path) == type(self.model_load_path) == type(self.tokenizer_load_path)
